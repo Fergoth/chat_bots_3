8f7f8d99e59f8d3fbda2d1a731ac046279ce941b
fergoth <fergothew@gmail.com>
2025-05-16 13:20:16 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,71 @@
+from dotenv import load_dotenv
+import os
+from google.cloud import dialogflow
+import logging
+from telegram import Update, ForceReply, Bot
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+)
+
+logger = logging.getLogger(__file__)
+
+
+def detect_intent_text(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, chat_id, tg_token):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot = Bot(token=tg_token)
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def response_bot(update: Update, context: CallbackContext):
+    project_id = os.environ["DIALOG_FLOW_PROJECT_ID"]
+    session_id = update.message.chat_id
+    language_code = "RU"
+    #TODO try exept
+    reply = detect_intent_text(
+        project_id, session_id, update.message.text, language_code
+    )
+    update.message.reply_text(reply)
+
+
+def main():
+    load_dotenv()
+    chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    tg_token = os.environ["TELEGRAM_BOT_TOKEN"]
+
+    tg_debug_token = os.environ.get("TELEGRAM_DEBUG_BOT_TOKEN")
+    if tg_debug_token:
+        logger.addHandler(TelegramLogsHandler(chat_id, tg_debug_token))
+    logger.setLevel(logging.INFO)
+    logger.info("Эхо Бот запущен")
+
+    updater = Updater(token=tg_token)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, response_bot)
+    )
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    print(main())
